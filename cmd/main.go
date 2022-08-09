a46b8388c7dfe8046807580dc154a97297f4ed68
orestonce <orestonce@163.com>
2022-08-09 21:02:55 +0800

    命令行支持合并一个目录下的ts文件

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/spf13/cobra"
+	"io/ioutil"
+	"log"
 	"m3u8d"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
 )
 
 var rootCmd = &cobra.Command{
@@ -50,6 +57,54 @@ var curlCmd = &cobra.Command{
 
 var gRunReq m3u8d.RunDownload_Req
 
+var gMergeReq struct {
+	InputTsDir    string
+	OutputMp4Name string
+}
+
+var mergeCmd = &cobra.Command{
+	Use: "merge",
+	Run: func(cmd *cobra.Command, args []string) {
+		if gMergeReq.InputTsDir == "" {
+			var err error
+			gMergeReq.InputTsDir, err = os.Getwd()
+			if err != nil {
+				log.Fatalln("获取当前目录失败")
+				return
+			}
+		}
+		fList, err := ioutil.ReadDir(gMergeReq.InputTsDir)
+		if err != nil {
+			log.Fatalln("读取目录失败", err)
+			return
+		}
+		var tsFileList []string
+		for _, f := range fList {
+			if f.Mode().IsRegular() && strings.HasSuffix(strings.ToLower(f.Name()), ".ts") {
+				tsFileList = append(tsFileList, filepath.Join(gMergeReq.InputTsDir, f.Name()))
+			}
+		}
+		if len(tsFileList) == 0 {
+			log.Fatalln("目录下不存在ts文件", gMergeReq.InputTsDir)
+			return
+		}
+		sort.Strings(tsFileList) // 按照字典顺序排序
+		if gMergeReq.OutputMp4Name == "" {
+			gMergeReq.OutputMp4Name = filepath.Join(gMergeReq.InputTsDir, "all.mp4")
+		}
+		err = m3u8d.MergeTsFileListToSingleMp4(m3u8d.MergeTsFileListToSingleMp4_Req{
+			TsFileList: tsFileList,
+			OutputMp4:  gMergeReq.OutputMp4Name,
+			Ctx:        context.Background(),
+		})
+		if err != nil {
+			log.Fatalln("合并失败", err)
+			return
+		}
+		log.Println("合并成功", gMergeReq.OutputMp4Name)
+	},
+}
+
 func init() {
 	downloadCmd.Flags().StringVarP(&gRunReq.M3u8Url, "M3u8Url", "u", "", "M3u8Url")
 	downloadCmd.Flags().BoolVarP(&gRunReq.Insecure, "Insecure", "", false, "是否允许不安全的请求")
@@ -61,6 +116,9 @@ func init() {
 	rootCmd.AddCommand(downloadCmd)
 	curlCmd.DisableFlagParsing = true
 	rootCmd.AddCommand(curlCmd)
+	mergeCmd.Flags().StringVarP(&gMergeReq.InputTsDir, "InputTsDir", "", "", "存放ts文件的目录(默认为当前工作目录)")
+	mergeCmd.Flags().StringVarP(&gMergeReq.OutputMp4Name, "OutputMp4Name", "", "", "输出mp4文件名(默认为输入ts文件的目录下的all.mp4)")
+	rootCmd.AddCommand(mergeCmd)
 }
 
 func main() {
