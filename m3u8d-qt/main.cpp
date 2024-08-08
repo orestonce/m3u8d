729d262154bbd05f851e3fa46f0ed1646f918dc0
orestonce <orestonce@163.com>
2024-08-08 08:00:56 +0800

    test-startup

--- m3u8d-qt/main.cpp
+++ m3u8d-qt/main.cpp
@@ -1,8 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
+    if(argc >= 2 && QString(argv[1]) == "test-startup")
+    {
+        //用于测试编译出来的二进制是否可以运行
+        std::cerr << "The test of startup.\n";
+        return 0;
+    }
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
